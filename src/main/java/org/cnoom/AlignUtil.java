578cf61c147b8f89b2e412c0c8a0806742d71a6a
WIN-U816FO4SA1Q\Administrator <1195582817@qq.com>
2022-12-27 14:30:15 +0800

    优化代码整洁度 GroupUtil.java 新增常用便捷方法

--- src/main/java/org/cnoom/AlignUtil.java
+++ src/main/java/org/cnoom/AlignUtil.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.utils.Align;
 
 public class AlignUtil {
 
-    public static float getAlignValueHorizontal(int align){
+    public static float getAlignValueHorizontal(int align) {
         if (Align.isLeft(align)) return 0;
         else if (Align.isRight(align)) return 1;
         return .5f;
     }
 
-    public static float getAlignValueVertical(int align){
+    public static float getAlignValueVertical(int align) {
         if (Align.isBottom(align)) return 0;
         else if (Align.isTop(align)) return 1;
         return .5f;
